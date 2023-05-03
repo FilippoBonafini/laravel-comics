ba8ec418184bad95999c23282339d249c43029ca
FilippoBonafini <filippo.bonafini.99@icloud.com>
2023-05-03 15:02:23 +0200

    sistemata grafica

--- resources/views/home.blade.php
+++
@@ -1,7 +0,0 @@
-@extends('layouts.app')
-
-@section('page.main')
-    <div>
-        <h1>Template Classe 89</h1>
-    </div>
-@endsection
--- resources/views/partials/header.blade.php
+++ resources/views/partials/header.blade.php
@@ -1,15 +1,55 @@
-<header>
+@php
+    $links = [
+        'characters' => [
+            'text' => 'Characters',
+            'destination' => '#',
+        ],
+        'comics' => [
+            'text' => 'Comics',
+            'destination' => '#',
+        ],
+        'movies' => [
+            'text' => 'Movies',
+            'destination' => '#',
+        ],
+        'tv' => [
+            'text' => 'Tv',
+            'destination' => '#',
+        ],
+        'games' => [
+            'text' => 'Games',
+            'destination' => '#',
+        ],
+        'collectibles' => [
+            'text' => 'Collectibles',
+            'destination' => '#',
+        ],
+        'videos' => [
+            'text' => 'Videos',
+            'destination' => '#',
+        ],
+        'fans' => [
+            'text' => 'Fans',
+            'destination' => '#',
+        ],
+        'news' => [
+            'text' => 'News',
+            'destination' => '#',
+        ],
+        'shop' => [
+            'text' => 'Shop',
+            'destination' => '#',
+        ],
+    ];
+@endphp
 
-    {{-- NAVBAR --}}
-    <nav>
-        @include('partials/partialsHeader.menu')
-    </nav>
-    {{-- /NAVBAR --}}
-
-    {{-- JUMBOTRON  --}}
-    <jumbo>
-        @include('partials/partialsHeader.jumbo')
-    </jumbo>
-    {{-- /JUMBOTRON  --}}
-
-</header>
+<div class="container">
+    <header>
+        <img src="{{ Vite::asset('resources/img/dc-logo.png') }}" alt="main-logo" class="main-logo">
+        <nav>
+            @foreach ($links as $link)
+            <a href="{{ $link['destination'] }}">{{ $link['text'] }}</a>
+            @endforeach
+        </nav>
+    </header>
+</div>
